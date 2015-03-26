a32ccd3336fe47b6c98e7c5ed6bb500e6d535596
Priyanka Goyal <priyanka.goyal@colorado.edu>
2015-03-25 23:27:28 -0600

    Create EveGuiConstants.java

---
+++ EveGuiConstants.java
@@ -0,0 +1,35 @@
+
+
+
+
+public interface EveGuiConstants {
+	String[] algorithmNamesArray = {"Huffman Compression",
+											//"Shannon Fano Compression",
+											//"GZip Compression",
+											//"Cosmo Compression",
+											//"JunkCode Binary Compression",
+											//"RLE Compressor",
+											//"LZW Compressor"
+											};
+
+	String[] extensionArray = {".huf",
+											//".sfe",
+											//".gz",
+											//".cos",
+											//".jbe",
+											//".rle",
+											//".lzw"
+											};
+
+	final int COMP_HUFFMAN = 0;
+	//final int COMP_SHANNONFANO = 1;
+	//final int COMP_GZIP = 2;
+	//final int COMP_COSMO = 3;
+	//final int COMP_JBC = 4;
+	//final int COMP_RLE = 5;
+	//final int COMP_LZW = 6;
+	
+	final int COMPRESS = 32;
+	final int DECOMPRESS = 33;
+	}
+
